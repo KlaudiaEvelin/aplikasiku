c94e47e6ad89a374ca18d88b8470e1ac57dec252
Amenokamiosa <amenokamiosaka@gmail.com>
2026-05-28 19:27:53 +0700

    try routing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome1');
+    return view('main');
+});
+
+// Di dalam routes/web.php
+Route::get('/', function () {
+    return view('home'); // akan memanggil file resources/views/home.blade.php
+});
+
+Route::get('/journal', function () {
+    return view('journal'); // akan memanggil file resources/views/journal.blade.php
 });
